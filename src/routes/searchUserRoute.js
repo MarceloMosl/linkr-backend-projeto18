b47cc133f2abccf8b3c6c8838bcc274a64d681ca
MarceloMosl <marcelomosl@icloud.com>
2023-03-09 21:11:46 -0300

    HotFix: AJuste no retorno do pesquisar por usuario

--- src/routes/searchUserRoute.js
+++ src/routes/searchUserRoute.js
@@ -7,33 +7,4 @@ const searchUserRoute = Router();
 searchUserRoute.get("/user/:id", validateToken, getUserPosts);
 searchUserRoute.post("/srcuser", postUsers);
 
-searchUserRoute.get("/teste", async (req, res) => {
-  const promise = await db.query(`SELECT
-      p.id,
-      p.user_id,
-      u.username,
-      u.user_url,
-      p.headline,
-      array_agg(z.name) AS hashtags_post,
-      array_agg(h.id) AS hashtags_id,
-      p.post_url,
-      COUNT(l.id) AS total_likes,
-      EXISTS(SELECT 1 FROM likes WHERE post_id = p.id AND user_id = 1) AS usuario_logado_like
-  FROM
-      posts p
-  INNER JOIN
-      users u ON p.user_id = u.id
-  LEFT JOIN
-      likes l ON p.id = l.post_id
-  LEFT JOIN
-      posts_hastags h ON p.id = h.post_id
-  LEFT JOIN 
-      hastags z ON z.id = h.hastag_id
-  GROUP BY
-      p.id,
-      u.id;`);
-
-  res.send(promise.rows);
-});
-
 export default searchUserRoute;
