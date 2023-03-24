9ba3c81a92edaf0adfc69015540015e1540c30a0
Karina <karinaalmeida523@gmail.com>
2023-03-24 19:51:00 -0300

    follow e unfollow rodando

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 
-import { login, signUp } from "../controllers/log.js";
+import { login, signUp, follow, unfollow } from "../controllers/log.js";
 import validateUser from "../middlewares/validateUser.js";
 import validateLogin from "../middlewares/validateLogin.js";
+import { validateToken } from "../middlewares/validateToken.js";
 
 import { loginSchema, signSchema } from "../schemas/loginSchema.js";
 
@@ -10,5 +11,7 @@ const userRoute = express.Router();
 
 userRoute.post("/sign-up", validateUser(signSchema), signUp);
 userRoute.post("/", validateLogin(loginSchema), login);
+userRoute.post("/user/:id", validateToken, follow )
+userRoute.delete("/user/:id", validateToken, unfollow)
 
 export default userRoute;
